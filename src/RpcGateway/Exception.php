cdce1dded47fb5be0bf9d59dc69e203d326eaa3a
Conrad Gülzow <conrad.guelzow@basilicom.de>
2016-03-11 10:25:41 +0100

    reformat code

--- src/RpcGateway/Exception.php
+++ src/RpcGateway/Exception.php
@@ -1,32 +1,31 @@
 <?php
 
-	namespace RpcGateway;
+namespace RpcGateway;
 
-	class Exception extends \Exception
-	{
-		/** @var mixed|null */
-		private $data = null;
+class Exception extends \Exception
+{
+    /** @var mixed|null */
+    private $data = null;
 
-		/**
-		 * Exception constructor.
-		 *
-		 * @param string          $message
-		 * @param int             $code
-		 * @param \Exception|null $previous
-		 * @param mixed|null      $data
-		 */
-		public function __construct($message = '', $code = 0, \Exception $previous = null, $data = null)
-		{
-			$this->data = $data;
-			parent::__construct($message, $code, $previous);
-		}
+    /**
+     * Exception constructor.
+     *
+     * @param string $message
+     * @param int $code
+     * @param \Exception|null $previous
+     * @param mixed|null $data
+     */
+    public function __construct($message = '', $code = 0, \Exception $previous = null, $data = null)
+    {
+        $this->data = $data;
+        parent::__construct($message, $code, $previous);
+    }
 
-		/**
-		 * @return mixed|null
-		 */
-		public function getData()
-		{
-			return $this->data;
-		}
-
-	}
+    /**
+     * @return mixed|null
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+}
